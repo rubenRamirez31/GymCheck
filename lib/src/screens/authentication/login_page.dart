8a91e8cfa055bcd2ad2914f2ac4d7f737792498a
rubenRamirez31 <l20te0221@teziutlan.tecnm.mx>
2024-04-13 06:39:12 -0600

    cambios

--- lib/src/screens/authentication/login_page.dart
+++ lib/src/screens/authentication/login_page.dart
@@ -1,122 +1,213 @@
 // ignore_for_file: use_build_context_synchronously
 
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:gym_check/src/components/app_text_form_field.dart';
 import 'package:gym_check/src/models/user_sesion_model.dart';
 import 'package:gym_check/src/providers/user_session_provider.dart';
+import 'package:gym_check/src/resources/resources.dart';
 import 'package:gym_check/src/services/user_service.dart';
+import 'package:gym_check/src/utils/common_widgets/gradient_background.dart';
+import 'package:gym_check/src/values/app_constants.dart';
+import 'package:gym_check/src/values/app_regex.dart';
+import 'package:gym_check/src/values/app_strings.dart';
+import 'package:gym_check/src/values/app_theme.dart';
 import 'package:provider/provider.dart';
 
 class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
   @override
+  // ignore: library_private_types_in_public_api
   _LoginPageState createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
-  final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
 
-  // Función para manejar el inicio de sesión
-  Future<void> _login(BuildContext context) async {
-    String email = _emailController.text.trim();
-    String password = _passwordController.text.trim();
+  final ValueNotifier<bool> passwordNotifier = ValueNotifier(true);
+  final ValueNotifier<bool> fieldValidNotifier = ValueNotifier(false);
 
-    // Realizar inicio de sesión usando ApiService
-    try {
-      Map<String, dynamic>? userData =
-          await UserService.loginUser(email, password);
-      if (userData != null) {
-        // Asigna el ID del usuario y el token obtenidos de la API
-        final userId = userData['userId'];
-        final token = userData['token'];
-        final userSession = UserSession(userId: userId, token: token);
-        Provider.of<UserSessionProvider>(context, listen: false)
-            .setUserSession(userSession);
+  late final TextEditingController emailController;
+  late final TextEditingController passwordController;
 
-        print(userId);
+  void initializeControllers() {
+    emailController = TextEditingController()..addListener(controllerListener);
+    passwordController = TextEditingController()
+      ..addListener(controllerListener);
+  }
 
-        Map<String, dynamic> userData2 = await UserService.getUserData(userId);
+  void disposeControllers() {
+    emailController.dispose();
+    passwordController.dispose();
+  }
 
-        int _primeros_pasos = userData2['primeros_pasos'];
-        print(_primeros_pasos);
+  void controllerListener() {
+    final email = emailController.text;
+    final password = passwordController.text;
 
-        if (_primeros_pasos == 0) {
-          // ignore: use_build_context_synchronously
-          Navigator.pushNamed(context, '/confirm_email');
-        } else if (_primeros_pasos == 1) {
-          Navigator.pushNamed(context, '/general_data');
-        } else if (_primeros_pasos == 2) {
-          Navigator.pushNamed(context, '/first_photo');
-        } else if (_primeros_pasos == 3) {
-          Navigator.pushNamed(context, '/body_data');
-        } else if (_primeros_pasos == 4) {
-          Navigator.pushNamed(context, '/nutritional_data');
-        } else if (_primeros_pasos == 5) {
-          Navigator.pushNamed(context, '/emotional_data');
-        } else if (_primeros_pasos == 6) {
-          Navigator.pushNamed(context, '/recomendar_premium');
-        } else if (_primeros_pasos == 7) {
-          Navigator.pushNamed(context, '/feed');
-        }
-      }
-    } catch (error) {
-      // Manejar el error en caso de fallo en el inicio de sesión
-      showDialog(
-        context: context,
-        builder: (context) => AlertDialog(
-          title: Text('Error'),
-          content: Text(
-              'Error al iniciar sesión. Por favor, revise su correo electrónico y contraseña.'),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () => Navigator.pop(context),
-              child: Text('OK'),
-            ),
-          ],
-        ),
-      );
+    if (email.isEmpty && password.isEmpty) return;
+
+    if (AppRegex.emailRegex.hasMatch(email) &&
+        AppRegex.passwordRegex.hasMatch(password)) {
+      fieldValidNotifier.value = true;
+    } else {
+      fieldValidNotifier.value = false;
     }
+  }
+
+  @override
+  void initState() {
+    initializeControllers();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    disposeControllers();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Inicio de Sesión'),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(
-                labelText: 'Correo Electrónico',
+      body: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          const GradientBackground(
+            children: [
+              Text(
+                AppStrings.signInToYourNAccount,
+                style: AppTheme.titleLarge,
+              ),
+              SizedBox(height: 6),
+              Text(AppStrings.signInToYourAccount, style: AppTheme.bodySmall),
+            ],
+          ),
+          Form(
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  AppTextFormField(
+                    controller: emailController,
+                    labelText: AppStrings.email,
+                    keyboardType: TextInputType.emailAddress,
+                    textInputAction: TextInputAction.next,
+                    onChanged: (_) => _formKey.currentState?.validate(),
+                    validator: (value) {
+                      return value!.isEmpty
+                          ? AppStrings.pleaseEnterEmailAddress
+                          : AppConstants.emailRegex.hasMatch(value)
+                              ? null
+                              : AppStrings.invalidEmailAddress;
+                    },
+                  ),
+                  ValueListenableBuilder(
+                    valueListenable: passwordNotifier,
+                    builder: (_, passwordObscure, __) {
+                      return AppTextFormField(
+                        obscureText: passwordObscure,
+                        controller: passwordController,
+                        labelText: AppStrings.password,
+                        textInputAction: TextInputAction.done,
+                        keyboardType: TextInputType.visiblePassword,
+                        onChanged: (_) => _formKey.currentState?.validate(),
+                        validator: (value) {
+                          return value!.isEmpty
+                              ? AppStrings.pleaseEnterPassword
+                              : AppConstants.passwordRegex.hasMatch(value)
+                                  ? null
+                                  : AppStrings.invalidPassword;
+                        },
+                        suffixIcon: IconButton(
+                          onPressed: () =>
+                              passwordNotifier.value = !passwordObscure,
+                          style: IconButton.styleFrom(
+                            minimumSize: const Size.square(48),
+                          ),
+                          icon: Icon(
+                            passwordObscure
+                                ? Icons.visibility_off_outlined
+                                : Icons.visibility_outlined,
+                            size: 20,
+                            color: Colors.black,
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text(AppStrings.forgotPassword),
+                  ),
+                  const SizedBox(height: 20),
+                  ValueListenableBuilder(
+                    valueListenable: fieldValidNotifier,
+                    builder: (_, isValid, __) {
+                      return FilledButton(
+                        onPressed: isValid
+                            ? () {
+                                emailController.clear();
+                                passwordController.clear();
+                              }
+                            : null,
+                        child: const Text(AppStrings.login),
+                      );
+                    },
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    children: [
+                      Expanded(child: Divider(color: Colors.grey.shade200)),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20),
+                        child: Text(
+                          AppStrings.orLoginWith,
+                          style: AppTheme.bodySmall.copyWith(
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                      Expanded(child: Divider(color: Colors.grey.shade200)),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: OutlinedButton.icon(
+                          onPressed: () {},
+                          icon: SvgPicture.asset(Vectors.google, width: 14),
+                          label: const Text(
+                            AppStrings.google,
+                            style: TextStyle(color: Colors.black),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 20),
+                      Expanded(
+                        child: OutlinedButton.icon(
+                          onPressed: () {},
+                          icon: SvgPicture.asset(Vectors.facebook, width: 14),
+                          label: const Text(
+                            AppStrings.facebook,
+                            style: TextStyle(color: Colors.black),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ),
-            SizedBox(height: 20.0),
-            TextField(
-              controller: _passwordController,
-              decoration: InputDecoration(
-                labelText: 'Contraseña',
-              ),
-              obscureText: true, // Ocultar texto de la contraseña
-            ),
-            SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: () => _login(context),
-              child: Text('Iniciar Sesión'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                // Navegar a la página de registro al hacer clic en el botón
-                Navigator.pushNamed(context, '/register');
-              },
-              child: Text('Registrarse'),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
+
+  // Función para manejar el inicio de sesión
 }
---
+++ lib/src/values/app_strings.dart
@@ -0,0 +1,40 @@
+class AppStrings {
+  const AppStrings._();
+
+  static const String loginAndRegister = 'Login and Register UI';
+  static const String uhOhPageNotFound = 'uh-oh!\nPage not found';
+  static const String register = 'Registrarse';
+  static const String login = 'Iniciar sesión';
+  static const String createYourAccount = 'Create your account';
+  static const String doNotHaveAnAccount = "Don't have an account?";
+  static const String facebook = 'Facebook';
+  static const String google = 'Google';
+  static const String signInToYourNAccount = 'Acceda a su \ncuenta';
+  static const String signInToYourAccount = 'Acceda a su cuenta';
+  static const String iHaveAnAccount = '¿Tengo una cuenta?';
+  static const String forgotPassword = '¿Ha olvidado su contraseña?';
+  static const String orLoginWith = 'o inicie sesión con';
+
+  static const String loggedIn = 'Logged In!';
+  static const String registrationComplete = 'Registro Completo';
+
+  static const String name = 'Nombre';
+  static const String pleaseEnterName = 'Porfavor, Ingrese un Nombre';
+  static const String invalidName = 'Nombre invalido';
+
+  static const String email = 'Correo';
+  static const String pleaseEnterEmailAddress =
+      'Por favor, introduzca su \ndirección de correo electrónico';
+  static const String invalidEmailAddress =
+      'Dirección de correo electrónico invalida';
+
+  static const String password = 'Contraseña';
+  static const String pleaseEnterPassword =
+      'Por favor, introduzca una contraseña';
+  static const String invalidPassword = 'Contraseña invalida';
+
+  static const String confirmPassword = 'Confirma Contraseña';
+  static const String pleaseReEnterPassword =
+      'Por favor, vuelva a introducir \nla contraseña';
+  static const String passwordNotMatched = 'Las Contraseñas no coinciden';
+}
