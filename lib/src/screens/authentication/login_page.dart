9d192952301a2d0d28ec071e6920f3449573fd9a
MeendyM <l20te0178@teziutlan.tecnm.mx>
2024-04-16 21:07:52 -0600

    Arreglando tus mamadas

--- lib/src/screens/authentication/login_page.dart
+++ lib/src/screens/authentication/login_page.dart
@@ -145,6 +145,13 @@ class _LoginPageState extends State<LoginPage> {
                     onPressed: () {},
                     child: const Text(AppStrings.forgotPassword),
                   ),
+                    TextButton(
+                    onPressed: () {
+                      _login(context);
+                    },
+                    child: const Text("ctmmmmm"),
+                  ),
+                  const SizedBox(height: 20),
                   const SizedBox(height: 20),
                   ValueListenableBuilder(
                     valueListenable: fieldValidNotifier,
