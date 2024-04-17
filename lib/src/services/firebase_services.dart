bdc017ea9d4309df38d9ac0d1e351461b4536617
rubenRamirez31 <l20te0221@teziutlan.tecnm.mx>
2024-04-17 14:47:54 -0600

    cambios de integracion a firebase

---
+++ lib/src/services/firebase_services.dart
@@ -0,0 +1,21 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:gym_check/src/models/social/post_model.dart';
+
+FirebaseFirestore db = FirebaseFirestore.instance;
+FirebaseAuth auth = FirebaseAuth.instance;
+
+Future<int> agregarPost(Post p) async {
+  CollectionReference coleccion = db.collection("Publicaciones");
+  int codigo = 0;
+
+  try {
+    await coleccion.add(p.toJson());
+    codigo = 200;
+  } catch (e) {
+    codigo = 500;
+  }
+
+  return codigo;
+}
