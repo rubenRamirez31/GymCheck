13da9a5fa7035e99b1bdbef422a7adfa1b80a6e6
rubenRamirez31 <l20te0221@teziutlan.tecnm.mx>
2024-04-18 12:06:23 -0600

    ok adios

--- lib/src/environments/environment.dart
+++ lib/src/environments/environment.dart
@@ -1,6 +1,6 @@
 class Environment {
   //static const String API_URL = 'http://172.16.20.85:3000';
-  static const String API_URL = 'http://192.168.1.82:3000';
+  static const String API_URL = 'http://192.168.17.12:3000';
 
   // Opcionalmente, puedes tener diferentes URLs para diferentes entornos
   // static const String apiUrl = 'http://api.tu-app.com'; // Para producción
