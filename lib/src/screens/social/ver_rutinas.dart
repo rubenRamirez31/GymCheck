13703d73008e0f0f75f64cf5f8b2f46d5143ca29
rubenRamirez31 <l20te0221@teziutlan.tecnm.mx>
2024-05-22 21:57:09 -0600

    iniciando creacion para la compartision de rutinas

---
+++ lib/src/screens/social/ver_rutinas.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class VerRutinas extends StatefulWidget {
+  const VerRutinas({super.key});
+
+  @override
+  State<VerRutinas> createState() => _VerRutinasState();
+}
+
+class _VerRutinasState extends State<VerRutinas> {
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(padding: EdgeInsets.all(10),
+    child: Column(
+      children: [
+        ListTile()
+      ],
+    ),);
+  }
+}
